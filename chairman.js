bc64791e1fe0d0cf83d96fb08a8d926fea01ca08
Ian De La Cruz <ridlc10@gmail.com>
2024-05-17 21:08:45 +0800

    Fix tooltips globally

--- chairman.js
+++ chairman.js
@@ -1,8 +1,3 @@
-Chart.defaults.plugins.tooltip.backgroundColor = '#FFFFFF'
-Chart.defaults.plugins.tooltip.titleColor = '#000'
-Chart.defaults.plugins.tooltip.bodyColor = '#000'
-Chart.defaults.plugins.tooltip.borderColor = '#90C8D8'
-Chart.defaults.plugins.tooltip.borderWidth = 1
 const yearLabels = [
   'Philippines', 'Vietnam', 'Indonesia',
   'Malaysia', 'Thailand', 'Singapore'
---
+++ globals.js
@@ -0,0 +1,5 @@
+Chart.defaults.plugins.tooltip.backgroundColor = '#FFFFFF'
+Chart.defaults.plugins.tooltip.titleColor = '#000'
+Chart.defaults.plugins.tooltip.bodyColor = '#000'
+Chart.defaults.plugins.tooltip.borderColor = '#90C8D8'
+Chart.defaults.plugins.tooltip.borderWidth = 1
