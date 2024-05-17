73f5260b2b1b981a66b2ca904392297dc21f3b04
Ian De La Cruz <ridlc10@gmail.com>
2024-05-17 13:38:17 +0800

    Fix behaviors

--- index.js
+++ index.js
@@ -152,6 +152,9 @@ let fyRevenue = new Chart(document.querySelector('#fyr'), {
     },
     responsive: true,
     plugins: {
+      legend: {
+        onClick: null,
+      },
       datalabels: {
         anchor: 'end',
         align: 'end',
@@ -176,7 +179,7 @@ let fyRevenue = new Chart(document.querySelector('#fyr'), {
 })
 
 let coreNI = new Chart(document.querySelector('#coreNI'), {
-  plugins: [coreLabel],
+  plugins: [ChartDataLabels, coreLabel],
   type: 'bar',
   data: {
     labels: yearLabels,
@@ -186,37 +189,62 @@ let coreNI = new Chart(document.querySelector('#coreNI'), {
         type: 'line',
         order: 0,
         backgroundColor: '#2C64CF',
-        borderColor: '#2C64CF'
+        borderColor: '#2C64CF',
+        borderRadius: 8
       },
       {
         label: 'JGSOC',
         data: [ -1.5, -2.4, -1.7, -13.8, -12.9 ],
-        backgroundColor: '#2DA574'
+        backgroundColor: '#2DA574',
+        datalabels: {
+          color: '#FFF6F8',
+        },
+        borderRadius: 8
       },
       {
         label: 'URC',
         data: [ 6.0, 6.4, 5.8, 6.4, 6.7 ],
         backgroundColor: '#DA3553',
+        datalabels: {
+          color: '#FFF6F8',
+        },
+        borderRadius: 8
       },
       {
         label: 'RLC',
         data: [ 5.3, 3.3, 4.8, 6.1, 8.2 ],
-        backgroundColor: '#004C97'
+        backgroundColor: '#004C97',
+        datalabels: {
+          color: '#FFF6F8',
+        },
+        borderRadius: 8
       },
       {
         label: 'CEB',
         data: [ 6.0, -14.8, -14.9, -7.3, 5.2 ],
-        backgroundColor: '#E7E670'
+        backgroundColor: '#E7E670',
+        datalabels: {
+          color: '#08358D',
+        },
+        borderRadius: 8
       },
       {
         label: 'RBank',
         data: [ 0.4, 0.6, 0.7, 0.8, 0.4 ],
-        backgroundColor: '#A8DB80'
+        backgroundColor: '#A8DB80',
+        datalabels: {
+          color: '#FFF6F8',
+        },
+        borderRadius: 8
       },
       {
         label: 'Others',
         data: [ 9.0, 7.4, 8.8, 14.0, 12.0 ],
-        backgroundColor: '#9AB8F4'
+        backgroundColor: '#9AB8F4',
+        datalabels: {
+          color: '#08358D',
+        },
+        borderRadius: 8
       },
     ],
   },
@@ -234,16 +262,31 @@ let coreNI = new Chart(document.querySelector('#coreNI'), {
       },
     },
     plugins: {
+      legend: {
+        onClick: null,
+        labels: {
+          filter: item => item.text,
+          usePointStyle: true,
+          boxWidth: 6
+        }
+      },
       datalabels: {
         anchor: 'end',
         align: 'end',
-        offset: 8,
+        offset: -28,
         font: {
           weight: 'bold',
           size: 14
         },
         formatter: function (value, context) {
             return value; // Display the actual data value
+        },
+        display: function(context) {
+          if (window.matchMedia('(hover:hover)').matches) {
+            return context.active
+          } else {
+            return true
+          }
         }
       }
     }
