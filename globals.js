e8915e79b40faef170f24c2cd6ee6edfe80ad75d
Ian De La Cruz <ridlc10@gmail.com>
2024-05-24 15:57:15 +0800

    Update global

--- globals.js
+++ globals.js
@@ -3,5 +3,5 @@ Chart.defaults.plugins.tooltip.titleColor = '#000'
 Chart.defaults.plugins.tooltip.bodyColor = '#000'
 Chart.defaults.plugins.tooltip.borderColor = '#90C8D8'
 Chart.defaults.plugins.tooltip.borderWidth = 1
-Chart.defaults.plugins.tooltip.font.family = "'Rethink Sans', sans-serif"
+Chart.defaults.plugins.tooltip.titleFont.family = "'Rethink Sans', sans-serif"
 Chart.defaults.font.family = "'Rethink Sans', sans-serif"
